1bd2abe159c2177a1b1b323f2543609d58504fa1
Michal Flaška <109202734+pilot2254@users.noreply.github.com>
2026-03-15 21:46:25 +0100

    chore: create BombermanDiscordManager class

---
+++ Bomberman3D/Source/Bomberman3D/Core/BombermanDiscordManager.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2026, Michal Flaška & RedFox Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "BombermanDiscordManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOMBERMAN3D_API UBombermanDiscordManager : public UObject
+{
+	GENERATED_BODY()
+	
+};
