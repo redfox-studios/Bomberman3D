1c44e6cffa317c20e3cfa59df5245609546b28f4
eugeniy3339 <122208682+eugeniy3339@users.noreply.github.com>
2026-03-30 09:48:56 +0200

    Update Bomberman3D.cpp

--- Bomberman3D/Source/Bomberman3D/Bomberman3D.cpp
+++ Bomberman3D/Source/Bomberman3D/Bomberman3D.cpp
@@ -4,3 +4,4 @@
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, Bomberman3D, "Bomberman3D");
+//caute negbebebe, tato hra je o gejoch, furikoch a skyro. ja milujem muzov a volam sa miso
