30b0d7b5299606fe880561f993e960b9e2f851d9
Michal Flaška <109202734+pilot2254@users.noreply.github.com>
2026-06-10 10:18:54 +0200

    feat: display current upgrades in HUD

--- Bomberman3D/Source/Bomberman3D/Core/BombermanGameMode.cpp
+++ Bomberman3D/Source/Bomberman3D/Core/BombermanGameMode.cpp
@@ -70,28 +70,6 @@ void ABombermanGameMode::Tick(float DeltaTime)
 			)
 		);
 	}
-
-	if (PS)
-	{
-		GEngine->AddOnScreenDebugMessage(1, 0.f, FColor::Cyan, FString::Printf(TEXT("Lives: %d | Score: %d"), PS->Lives, (int32)PS->GetScore()));
-
-		GEngine->AddOnScreenDebugMessage(
-			2,
-			0.f,
-			FColor::Green,
-			FString::Printf(
-				TEXT("BombUp: %d | FireUp: %d | SpeedUp: %d | Invincible: %d | WallPass: %d | BombPass: %d | FlamePass: %d | RemoteControl: %d"),
-				PS->Upgrades.BombUp,
-				PS->Upgrades.FireUp,
-				PS->Upgrades.SpeedUp,
-				PS->Upgrades.bInvincible,
-				PS->Upgrades.bWallPass,
-				PS->Upgrades.bBombPass,
-				PS->Upgrades.bFlamePass,
-				PS->Upgrades.bRemoteControl
-			)
-		);
-	}
 }
 
 void ABombermanGameMode::BeginPlay()
