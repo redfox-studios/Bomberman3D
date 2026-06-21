a9f7842abd94ea0744609be0182bcc9a0f361ae7
Michal Flaška <109202734+pilot2254@users.noreply.github.com>
2026-06-21 20:04:44 +0200

    feat: allow logging in shipping build

--- Bomberman3D/Source/Bomberman3D.Target.cs
+++ Bomberman3D/Source/Bomberman3D.Target.cs
@@ -11,5 +11,9 @@ public class Bomberman3DTarget : TargetRules
 		DefaultBuildSettings = BuildSettingsVersion.V5;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
 		ExtraModuleNames.Add("Bomberman3D");
-	}
+
+        //https://dev.epicgames.com/community/learning/knowledge-base/vzvZ/unreal-engine-enabling-logging-in-shipping-builds
+        BuildEnvironment = TargetBuildEnvironment.Unique;
+        bUseLoggingInShipping = true;
+    }
 }
